5920f53e17808e5ad78d2957b2e589511f1752ee
Aditi Singh <64026259+aditisingh0409@users.noreply.github.com>
2024-02-20 10:14:21 +0530

    Update CalculatorMt2023085Application.java

--- src/main/java/com/example/calculator_mt2023085/CalculatorMt2023085Application.java
+++ src/main/java/com/example/calculator_mt2023085/CalculatorMt2023085Application.java
@@ -1,6 +1,5 @@
 package com.example.calculator_mt2023085;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Scanner;
 
@@ -23,41 +22,48 @@ public class CalculatorMt2023085Application {
         return num1/num2;
     }
     public static void main(String[] args) {
-        // Main calc = new Main();
-        System.out.println("Please select the operation from below: ");
-        System.out.println("1. Add ");
-        System.out.println("2. Subtract ");
-        System.out.println("3. Multiply ");
-        System.out.println("4. Division ");
-        System.out.println("5. Exit ");
-        //H
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        int choice = 0;
+        while(true) 
+        {
+            System.out.println("Please select the operation from below: ");
+            System.out.println("1. Add ");
+            System.out.println("2. Subtract ");
+            System.out.println("3. Multiply ");
+            System.out.println("4. Division ");
+            System.out.println("5. Exit ");
 
-        System.out.println("Enter Choice: ");
-        int choice = myObj.nextInt();  // Read user input
-
-        System.out.println("Enter the 2 numbers: ");
-        int num1 = myObj.nextInt();  // Read user input
-        int num2 = myObj.nextInt();  // Read user input
-        int ans=0;
-        switch(choice) {
-            case 1:
-                ans=add(num1,num2);
-                break;
-            case 2:
-                ans=subtract(num1,num2);
-                break;
-            case 3:
-                ans=multiply(num1,num2);
-                break;
-            case 4:
-                ans=division(num1,num2);
-                break;
-            case 5:
-                myObj.close();
-                return;
+            // Creating a Scanner object
+            Scanner input = new Scanner(System.in);  
+            System.out.println("Enter Choice: ");
+            
+            // Reading User Input
+            choice = input.nextInt();  
+            if(choice == 5) 
+                System.exit(0);
+    
+            System.out.println("Enter the 2 numbers: ");
+            int num1 = input.nextInt();  
+            int num2 = input.nextInt();  
+            int ans=0;
+            switch(choice) {
+                case 1:
+                    ans=add(num1,num2);
+                    break;
+                case 2:
+                    ans=subtract(num1,num2);
+                    break;
+                case 3:
+                    ans=multiply(num1,num2);
+                    break;
+                case 4:
+                    ans=division(num1,num2);
+                    break;
+                default:
+                    System.exit(0);
+                    input.close();
+                }
+            System.out.println("The answer is = "+ans);
+            input.close();
         }
-        System.out.println("The answer is = "+ans);
-        myObj.close();
     }
 }
